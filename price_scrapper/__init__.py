5a3d393567aa19b6d0609ca6b73d24b8963062fa
Noeh <noehblabla33@gmail.com>
2025-12-05 18:55:43 +0100

    Scrapper de prix retiré pour le moment

--- price_scrapper/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Module de collecte et gestion des prix d'ingrédients
-"""
-from price_scraper.config import ScraperConfig
-from price_scraper.models import PrixHistorique, SourcePrix, MappingIngredient
-
-__version__ = '1.0.0'
-__all__ = ['ScraperConfig', 'PrixHistorique', 'SourcePrix', 'MappingIngredient']
--- price_scrapper/scrapers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Scrapers pour différentes sources de prix
-"""
-from price_scraper.scrapers.base_scraper import BaseScraper
-from price_scraper.scrapers.openfoodfacts import OpenFoodFactsScraper
-
-__all__ = ['BaseScraper', 'OpenFoodFactsScraper']
